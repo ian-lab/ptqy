87d2b13e138ca56fc92e972828901441cb0a94ff
ian-lab <ianfyb123@gmail.com>
2020-11-08 22:17:34 +0800

    update

--- word2pdf/main.py
+++ word2pdf/main.py
@@ -3,8 +3,6 @@ import os
 import re
 from word2pdf import Ui_MainWindow
 from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
-import PyQt5.QtGui as qg
-from win32com.client import gencache
 from win32com.client import constants, gencache
 
 count = 0
@@ -14,9 +12,19 @@ directory = ''
 
 class trans(QMainWindow, Ui_MainWindow):  # 继承自父类QtWidgets.QMainWindow
     def __init__(self, parent=None):
+        global directory, count, num_file
+        count = 0
+        num_file = 0
         super(trans, self).__init__()  # 因为继承关系，要对父类初始化
         # 通过super初始化父类，__init__()函数无self，若直接QtWidgets.QMainWindow).__init__(self)，括号里是有self的
         self.setupUi(self)
+        self.setFixedSize(self.width(), self.height())
+        self.progressBar.setValue(0)
+        directory = os.getcwd()
+        directory = directory.replace('\\', '/')
+        self.FilePathText.setPlainText(directory)
+        directory = directory.replace('/', '\\')
+
         self.word2pdfButton.clicked.connect(self.word2pdfButton_clicked)
         self.excel2pdfButton.clicked.connect(self.excel2pdfButton_clicked)
         self.openFileButton.clicked.connect(self.openFileButton_clicked)
@@ -34,7 +42,7 @@ class trans(QMainWindow, Ui_MainWindow):  # 继承自父类QtWidgets.QMainWindow
         doc = word.Documents.Open(wordPath, ReadOnly=1)
         doc.ExportAsFixedFormat(pdfPath,
                                 constants.wdExportFormatPDF,
-                                # OptimizeFor=constants.wdExportOptimizeForPrint,
+                                OptimizeFor=constants.wdExportOptimizeForPrint,
                                 Item=constants.wdExportDocumentWithMarkup,
                                 CreateBookmarks=constants.wdExportCreateHeadingBookmarks)
         word.Quit(constants.wdDoNotSaveChanges)
@@ -58,22 +66,26 @@ class trans(QMainWindow, Ui_MainWindow):  # 继承自父类QtWidgets.QMainWindow
                 global num_file
                 if re.search('\.(doc|docx)', name):
                     num_file += 1
-        self.textBrowser.setPlainText(("共找到 %d 个Word文件，转换中..." % (num_file)))
-        for dirs, subdirs, files in os.walk(directory):
-            for name in files:
-                if re.search('\.(doc|docx)', name):
-                    global count
-                    count = count + 1
-                    self.textBrowser.append(
-                        ("第 %d / %d 个文件..." % (count, num_file)))
-                    self.textBrowser.moveCursor(
-                        self.textBrowser.textCursor().End)  # 文本框显示到底部
-                    QApplication.processEvents()
-                    self.word2pdf(dirs+'\\'+name, dirs+'\\' +
-                                  re.subn('(docx|doc)', 'pdf', name)[0])
-                    self.progressBar.setValue(count / num_file * 100)
-        self.textBrowser.append('转换完成')
-        self.progressBar.setValue(100)
+        if num_file == 0:
+            self.textBrowser.setPlainText('未找到Word文件，请确认文件路径')
+        else:
+            self.textBrowser.setPlainText(
+                ("共找到 %d 个Word文件，开始转换" % (num_file)))
+            for dirs, subdirs, files in os.walk(directory):
+                for name in files:
+                    if re.search('\.(doc|docx)', name):
+                        global count
+                        count = count + 1
+                        self.textBrowser.append(
+                            ("第 %d / %d 个文件转换中..." % (count, num_file)))
+                        self.textBrowser.moveCursor(
+                            self.textBrowser.textCursor().End)  # 文本框显示到底部
+                        QApplication.processEvents()
+                        self.word2pdf(dirs+'\\'+name, dirs+'\\' +
+                                      re.subn('(docx|doc)', 'pdf', name)[0])
+                        self.progressBar.setValue(count / num_file * 100)
+            self.textBrowser.append('转换完成')
+            self.progressBar.setValue(100)
         count = 0
         num_file = 0
         self.word2pdfButton.setEnabled(True)
@@ -90,22 +102,26 @@ class trans(QMainWindow, Ui_MainWindow):  # 继承自父类QtWidgets.QMainWindow
                 global num_file
                 if re.search('\.(xlsx|xls)', name):
                     num_file += 1
-        self.textBrowser.setPlainText(("共找到 %d 个Excel文件，转换中..." % (num_file)))
-        for dirs, subdirs, files in os.walk(directory):
-            for name in files:
-                if re.search('\.(xlsx|xls)', name):
-                    global count
-                    count = count + 1
-                    self.textBrowser.append(
-                        ("第 %d / %d 个文件..." % (count, num_file)))
-                    self.textBrowser.moveCursor(
-                        self.textBrowser.textCursor().End)  # 文本框显示到底部
-                    QApplication.processEvents()
-                    self.excel2pdf(dirs+'\\'+name, dirs+'\\' +
-                                   re.subn('(xlsx|xls)', 'pdf', name)[0])
-                    self.progressBar.setValue(count/num_file*100)
-        self.textBrowser.append('转换完成')
-        self.progressBar.setValue(100)
+        if num_file == 0:
+            self.textBrowser.setPlainText('未找到Excel文件，请确认文件路径')
+        else:
+            self.textBrowser.setPlainText(
+                ("共找到 %d 个Excel文件，开始转换" % (num_file)))
+            for dirs, subdirs, files in os.walk(directory):
+                for name in files:
+                    if re.search('\.(xlsx|xls)', name):
+                        global count
+                        count = count + 1
+                        self.textBrowser.append(
+                            ("第 %d / %d 个文件转换中..." % (count, num_file)))
+                        self.textBrowser.moveCursor(
+                            self.textBrowser.textCursor().End)  # 文本框显示到底部
+                        QApplication.processEvents()
+                        self.excel2pdf(dirs+'\\'+name, dirs+'\\' +
+                                       re.subn('(xlsx|xls)', 'pdf', name)[0])
+                        self.progressBar.setValue(count/num_file*100)
+            self.textBrowser.append('转换完成')
+            self.progressBar.setValue(100)
         count = 0
         num_file = 0
         self.word2pdfButton.setEnabled(True)
